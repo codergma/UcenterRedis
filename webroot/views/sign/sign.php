f464b64d4c03fb2aab0bb64aa2694d34ea6a5da7
liubin <liubin@mapgis.com>
2015-11-03 19:15:22 +0800

    *

---
+++ webroot/views/sign/sign.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>登录</title>
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+  <div class="container">
+  	<div class="logo"></div>
+  	<div class="title"></div>
+  	<div class="sign-container"></div>
+  	<div class="sign-sns"></div>
+  </div>
+
+    <script src="/jquery/jquery-1.11.3.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
