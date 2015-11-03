c5f6e93cc85c152cee1da007d6c030405237ecff
codergma <codergma@163.com>
2015-11-03 21:21:25 +0800

    *

--- webroot/views/sign/sign.php
+++ webroot/views/sign/sign.php
@@ -6,13 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>登录</title>
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+    .signin-page{
+      padding-top: 125px;
+      padding-bottom: 50px;
+      width: 940px;
+      margin: 0 auto;
+    }
+    .logo{
+      font-size: 150px;
+      color: pink;
+    }
+    .title{
+      margin: 70px 0 50px;
+      border-bottom: solid 1px #eeeeee;
+    }
+    .title span{
+      position: relative;
+      top:10px;
+      background-color: rgba(255,255,255,1);
+    }
+    .title a{
+      color: #b1b1b1;
+      padding: 0 5px;
+    }
+    .title a:link,a:hover,a:active,a:visited{
+      text-decoration: none;
+    }
+    .title b{
+      padding: 0 5px;
+    }
+    </style>
   </head>
   <body>
   <div class="container">
-  	<div class="logo"></div>
-  	<div class="title"></div>
-  	<div class="sign-container"></div>
-  	<div class="sign-sns"></div>
+    <div class="signin-page">
+    	<div class="logo text-center">
+       share 
+      </div>
+    	<h4 class="title text-center">
+        <span>
+          <a  href="##">登录</a>
+          <b class="text-muted">·</b>
+          <a href="##">注册</a>
+        </span>
+      </h4>
+    	<div class="sign-container"></div>
+    	<div class="sign-sns"></div>
+    </div>
   </div>
 
     <script src="/jquery/jquery-1.11.3.min.js"></script>
