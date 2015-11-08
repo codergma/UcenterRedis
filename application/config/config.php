d7c4ec5a830702a93d8f0545c0dadca215f66673
liubin <liubin@mapgis.com>
2015-11-08 15:27:44 +0800

    *

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:8084';
+$config['base_url'] = 'http://codergma.com:8084';
 
 /*
 |--------------------------------------------------------------------------
