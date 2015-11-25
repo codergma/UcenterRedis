6a95037de47b6aa89528c96d3bae6b0a1830a601
codergma <codergma@163.com>
2015-11-25 10:04:06 +0800

    重构

--- application/controllers/Sign.php
+++ application/controllers/Sign.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') or die('No direct script access allowed');
 define('CAPTCHA_EXPIRE',60);
+
 class Sign extends CI_Controller{
 	private $redis = null;
 	private $error_msg = array(
@@ -32,7 +33,7 @@ class Sign extends CI_Controller{
 	public function index()
 	{
 		// 检查是否已经登陆
-		$is_signin = $this->check_signin();
+		$is_signin = $this->_check_signin();
 		
 		if ($is_signin) 
 		{
@@ -55,7 +56,12 @@ class Sign extends CI_Controller{
         $this->load->view('sign/modify_password.php');
 	}
 
-	//注册接口
+	/**
+	* signup
+	* 注册新用接口
+	*
+	*　@return json
+	*/
 	public function signup()
 	{
 		$signup_email     = $this->input->post('signup-email');
@@ -69,7 +75,7 @@ class Sign extends CI_Controller{
 		$password  = addslashes(trim($signup_password));
         $captcha   = strtolower($signup_captcha);
 
-        if(!$this->validate_signup($email,$username,$password,$captcha))
+        if(!$this->_validate_signup($email,$username,$password,$captcha))
         {
         	return false;
         }
@@ -80,58 +86,63 @@ class Sign extends CI_Controller{
 		$this->cg_base->echo_json($result,"success");
 	}
 
-    /*
-	*用户名和口令：
-			正则表达式限制用户输入口令；
-			密码加密保存 md5(md5(passwd+salt))；
-			允许浏览器保存口令；
-			口令在网上传输协议http;
-	*用户登陆状态：
-			因为http是无状态的协议，每次请求都是独立的， 所以这个协议无
-			法记录用户访问状态，在多个页面跳转中如何知道用户登陆状态呢?
-			那就要在每个页面都要对用户身份进行认证。
-			我将使用三种方法实现用户登录状态验证:
-			(1)在cookie中保存用户名和密码，每次页面跳转的时候进行密码验证，正确则登录状态；
-			   这个方法显然太挫了，每次都要与数据库交互显然严重影响性能，那么你可能想，直接
-			   在cookie中保存登录状态true|false，这更挫，太不安全了，不过你可以在session
-			   中保存，这就是第二种实现方法。
-			(2)在session中保存登陆状态true|false|更多信息,在页面跳转的时候获取session即可，
-			   因为session是保存在服务器中的， 所以相对安全一些,但是由于默认session是保存在
-			   文件中的，所以当用户数量上来之后，会产生大量小文件，影响系统性能，当然你可以更
-			   换文件系统，或者指定其他存储方式，比如数据库。
-			(3)解决上面所说的性能问题，这时候就要用到Inmemory的key-value型数据库了，以前memcache
-				用的比较广泛，现在redis等越来越火了。
-	*使用cookie的一些原则：
-		(1)cookie中保存用户名，登录序列，登录token;
-				用户名：明文；
-				登录序列：md5散列过的随机数,仅当强制用户输入口令时更新;
-				登陆token：md5散列过的随机数，仅一个登陆session内有效，新的session会更新他。
-		(2)上述三个东西会存放在服务器上，服务器会验证客户端cookie与服务器是否一致；
-		(3)这样设计的效果
-				(a)登录token是单实例，一个用户只能有一个登录实例；
-				(b)登录序列用来做盗用行为检测
-	*找回密码功能
-		(1)不要使用安全问答
-		(2)通过邮件自行重置。当用户申请找回密码时，系统生成一个md5唯一的随机字串
-		　放在数据库中，然后设置上时限，给用户发一个邮件，这个链接中包含那个md5
-		　，用户通过点击那个链接来自己重置新的口令。
-		(3)更好的做法多重认证。
-	*口令探测防守
-		(1)验证码
-		(2)用户口令失败次数,并且增加尝试的时间成本
-		(3)系统全局防守,比如系统每天5000次口令错误，就认为遭遇了攻击，
-	  	然后增加所有用户输错口令的时间成本。
-		(4)使用第三方的OAuth和OpenID,现在很少用了。
+    /**
+	* 用户名和口令：
+	*		正则表达式限制用户输入口令；
+	*		密码加密保存 md5(md5(passwd+salt))；
+	*		允许浏览器保存口令；
+	*		口令在网上传输协议http;
+	* 用户登陆状态：
+	*		因为http是无状态的协议，每次请求都是独立的， 所以这个协议无
+	*		法记录用户访问状态，在多个页面跳转中如何知道用户登陆状态呢?
+	*		那就要在每个页面都要对用户身份进行认证。
+	*		我将使用三种方法实现用户登录状态验证:
+	*		(1)在cookie中保存用户名和密码，每次页面跳转的时候进行密码验证，正确则登录状态；
+	*		   这个方法显然太挫了，每次都要与数据库交互显然严重影响性能，那么你可能想，直接
+	*		   在cookie中保存登录状态true|false，这更挫，太不安全了，不过你可以在session
+	*		   中保存，这就是第二种实现方法。
+	*		(2)在session中保存登陆状态true|false|更多信息,在页面跳转的时候获取session即可，
+	*		   因为session是保存在服务器中的， 所以相对安全一些,但是由于默认session是保存在
+	*		   文件中的，所以当用户数量上来之后，会产生大量小文件，影响系统性能，当然你可以更
+	*		   换文件系统，或者指定其他存储方式，比如数据库。
+	*		(3)解决上面所说的性能问题，这时候就要用到Inmemory的key-value型数据库了，以前memcache
+	*			用的比较广泛，现在redis等越来越火了。
+	* 使用cookie的一些原则：
+	*	(1)cookie中保存用户名，登录序列，登录token;
+	*			用户名：明文；
+	*			登录序列：md5散列过的随机数,仅当强制用户输入口令时更新;
+	*			登陆token：md5散列过的随机数，仅一个登陆session内有效，新的session会更新他。
+	*	(2)上述三个东西会存放在服务器上，服务器会验证客户端cookie与服务器是否一致；
+	*	(3)这样设计的效果
+	*			(a)登录token是单实例，一个用户只能有一个登录实例；
+	*			(b)登录序列用来做盗用行为检测
+	* 找回密码功能
+	*	(1)不要使用安全问答
+	*	(2)通过邮件自行重置。当用户申请找回密码时，系统生成一个md5唯一的随机字串
+	*	　放在数据库中，然后设置上时限，给用户发一个邮件，这个链接中包含那个md5
+	*	　，用户通过点击那个链接来自己重置新的口令。
+	*	(3)更好的做法多重认证。
+	* 口令探测防守
+	*	(1)验证码
+	*	(2)用户口令失败次数,并且增加尝试的时间成本
+	*	(3)系统全局防守,比如系统每天5000次口令错误，就认为遭遇了攻击，
+	*  	然后增加所有用户输错口令的时间成本。
+	*	(4)使用第三方的OAuth和OpenID,现在很少用了。
 	*/
 
-	//登录接口
+	/**
+	* sinin
+	* 登录接口
+	*
+	* @return json
+	*/
 	public function signin()
 	{
 		$signin_username   = addslashes(trim($this->input->post('signin-username')));
 		$signin_password   = addslashes(trim($this->input->post('signin-password')));
 		$signin_captcha    = strtolower($this->input->post('signin-captcha'));
 
-		$validation = $this->validate_signin($signin_username,$signin_password,$signin_captcha);
+		$validation = $this->_validate_signin($signin_username,$signin_password,$signin_captcha);
 		if (!$validation)
 		{
 			return ;
@@ -145,11 +156,11 @@ class Sign extends CI_Controller{
           return;
 		} 
 		//检查登录错误次数
-		$fail_num = $this->get_fail_count($signin_username);
+		$fail_num = $this->_get_fail_count($signin_username);
 		if ($fail_num >= MAX_ERR_NUM)
 		{
-			$this->set_fail_count($signin_username);
-			$ttl = $this->get_ttl($signin_username);
+			$this->_set_fail_count($signin_username);
+			$ttl = $this->_get_ttl($signin_username);
 			$ttl = ceil($ttl/60);
 			$this->cg_base->echo_json(-6,$this->error_msg[-6].", 请{$ttl}分钟后重试");
             return ;
@@ -170,20 +181,25 @@ class Sign extends CI_Controller{
 				"email"=>$user->email
 				);
 			$this->redis->setex($key,EXPIRE,json_encode($value));
-			$this->reset_fail_count($signin_username);
+			$this->_reset_fail_count($signin_username);
 
 		    $this->cg_base->echo_json(1,$this->error_msg[1]);
 		}
 		else
 		{
-			$this->set_fail_count($signin_username);
-			$count = 5 - $this->get_fail_count($signin_username);
+			$this->_set_fail_count($signin_username);
+			$count = 5 - $this->_get_fail_count($signin_username);
 		    $this->cg_base->echo_json(-7,$this->error_msg[-7].", 还剩{$count}次机会");
             return ;
 		}
 	}
 
-	//登出
+	/**
+	* signout
+	* 登出接口
+	*
+	* @return json
+	*/
 	public function signout()
 	{
 		if (isset($_COOKIE['uid']))
@@ -194,6 +210,12 @@ class Sign extends CI_Controller{
 	}
 
 	//发送修改密码链接
+	/**
+	* modify_password
+	* 发送修改密码链接到用户邮箱
+	*
+	* ＠return json
+	*/
 	public function modify_password()
 	{
 		$email = $this->input->post('email');
@@ -227,7 +249,12 @@ class Sign extends CI_Controller{
 		}
 
 	}
-	//重置密码
+	/**
+	* reset_passwd
+	* 重置密码操作接口
+	*
+	* @return json
+	*/
 	public function reset_passwd($reset_flag)
 	{
         if(empty($reset_flag))
@@ -237,7 +264,7 @@ class Sign extends CI_Controller{
         }
 
 		$passwd = $this->input->post_get('password');
-		if (!$this->check_password_formate($passwd))
+		if (!$this->_check_password_formate($passwd))
 		{
 			$this->cg_base->echo_json(-1,'fail');
 			return;
@@ -261,20 +288,25 @@ class Sign extends CI_Controller{
 
 	}
 
-	//验证注册数据是否合法
-	protected function validate_signup($email,$username,$password,$captcha)
+	/**
+	* _validate_signup
+	* 验证注册信息是否合法
+	*
+	* @return bool
+	*/
+	private function _validate_signup($email,$username,$password,$captcha)
 	{
 		if (empty($email))
 		{
 			$this->cg_base->echo_json(-8,$this->error_msg[-8]);
 			return false;
 		}
-		if (!$this->check_email_formate($email)) 
+		if (!$this->_check_email_formate($email)) 
 		{
 			$this->cg_base->echo_json(-9,$this->error_msg[-9]);
 			return false;
 		}
-		if(!$this->validate_signin($username,$password,$captcha))
+		if(!$this->_validate_signin($username,$password,$captcha))
 		{
 			return false;
 		}
@@ -293,8 +325,13 @@ class Sign extends CI_Controller{
 		
 		return true;
 	}
-	//检查用户登录数据合法性
-	protected function validate_signin($username,$password,$captcha)
+	/**
+	* _validate_signin
+	* 验证用户登录数据合法性
+	*
+	* @return bool
+	*/
+	private function _validate_signin($username,$password,$captcha)
 	{
 		if (empty($username))
 		{
@@ -306,12 +343,12 @@ class Sign extends CI_Controller{
 			$this->cg_base->echo_json(-2,$this->error_msg[-2]);
 			return false;
 		}
-		if (!$this->check_username_formate($username))
+		if (!$this->_check_username_formate($username))
 		{
 			$this->cg_base->echo_json(-3,$this->error_msg[-3]);
 			return false;
 		}
-		if (!$this->check_password_formate($password))
+		if (!$this->_check_password_formate($password))
 		{
 			$this->cg_base->echo_json(-4,$this->error_msg[-4]);
 			return false;
@@ -327,26 +364,46 @@ class Sign extends CI_Controller{
 		}
 		return true;
 	}
-	//检查用户名格式
-	public function check_username_formate($username)
+	/**
+	* _check_username_formate
+	* 检查用户名格式
+	*
+	* @return bool
+	*/
+	private function _check_username_formate($username)
 	{
 		$res = preg_match('/^[0-9A-Za-z_]{6,32}$/', $username);
 		return $res ? true:false;
 	}
-	//检查邮件格式
-	public function check_email_formate($email)
+	/**
+	* _check_email_formate
+	* 检查邮件格式
+	*
+	* @return bool
+	*/
+	private function _check_email_formate($email)
 	{
     	$res = preg_match('/^([0-9A-Za-z]+)([0-9a-zA-Z_-]*)@([0-9A-Za-z]+).([A-Za-z]+)$/',$email);
     	return $res;
 	}
-	//检查密码格式
-	public function check_password_formate($password)
+	/**
+	* _check_email_formate
+	* 检查密码格式
+	*
+	* @return bool
+	*/
+	private function _check_password_formate($password)
 	{
 	    $res = preg_match('/[a-zA-Z]+/', $password) && preg_match('/[0-9]+/',$password) && preg_match('/[\s\S]{6,16}$/',$password);
 	    return $res;
 	}
-	//根据redis判断用户是否在线
-	public function check_signin()
+	/**
+	* _check_signin
+	* 根据redis判断用户是否在线
+	*
+	* @return bool
+	*/
+	private function _check_signin()
 	{
 		//redis
 		if (isset($_COOKIE['uid'])) {
@@ -355,8 +412,14 @@ class Sign extends CI_Controller{
 			return false;
 		}
 	}
-	//登录失败，错误数加一,expire为30分钟
-	protected function set_fail_count($username)
+	/**
+	* _set_fail_count
+	* 登录失败，错误数加一,expire为30分钟
+	*
+	* @param  string  用户名
+	* @return void
+	*/
+	private function _set_fail_count($username)
 	{
 		$key = md5($username.'sign_fail');
 
@@ -368,8 +431,14 @@ class Sign extends CI_Controller{
 			$this->redis->setex($key,EXPIRE,1);
 		}
 	}
-	//当前登陆错误次数　
-	protected function get_fail_count($username)
+	/**
+	* _get_fail_count
+	* 当前登陆错误次数　
+	*
+	* @param string 用户名
+	* @return int
+	*/
+	private function _get_fail_count($username)
 	{
 		$key = md5($username.'sign_fail');
 		if ($this->redis->exists($key))
@@ -380,8 +449,14 @@ class Sign extends CI_Controller{
 			return 0;
 		}
 	}
-	//登录成功，重置登录错误次数　
-	protected function reset_fail_count($username)
+	/**
+	* _reset_fail_count
+	* 登录成功，重置登录错误次数　
+	*
+	* @param string 用户名
+	* @return bool
+	*/
+	private function _reset_fail_count($username)
 	{
 		$key = md5($username.'sign_fail');
 		if ($this->redis->exists($key))
@@ -389,11 +464,18 @@ class Sign extends CI_Controller{
 			return $this->redis->setex($key,EXPIRE,0);
 		}else
 		{
-			return 0;
+			return FALSE;
 		}
 	}
-	//redis中记录错误次数的键值对过期时间
-	protected function get_ttl($username)
+	//
+	/**
+	* _get_ttl
+	* redis中记录错误次数的键值对过期时间
+	*
+	* @param string 用户名
+	* @return int  
+	*/
+	private function _get_ttl($username)
 	{
 		$key = md5($username.'sign_fail');
 		if ($this->redis->exists($key))
@@ -406,10 +488,13 @@ class Sign extends CI_Controller{
 
 	}
 	/**
-	 *　生成验证码图片，并将验证码信息保存到redis的hash结构中
-	 * 数据结构：　ip是key,
-	 * word作为filed,验证码内容作value,记录验证码字符串
-	 * times作为filed,次数作为value,记录次ip尝试获取验证码次数
+	* _gen_captcha
+	*　生成验证码图片，并将验证码信息保存到redis的hash结构中
+	* 数据结构：　ip是key,
+	* word作为filed,验证码内容作value,记录验证码字符串
+	* times作为filed,次数作为value,记录次ip尝试获取验证码次数
+	*
+	* @return captcha
 	*/
 	private function _gen_captcha()
 	{
@@ -449,13 +534,17 @@ class Sign extends CI_Controller{
 		}
 		return $cap;
 	}
-	//生成验证码对外接口
+	/*
+	* gen_captcha
+	* 生成验证码对外接口
+	*
+	* @return json
+	*/
 	public function gen_captcha()
 	{
 		$cap = $this->_gen_captcha();
 		$this->cg_base->echo_json(true,"success",$cap);
 	}
-
 
 
 }
